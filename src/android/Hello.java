c542fe456d35f9f55bed136655cbce03662fa36f
Tomas Radvansky <tomas.radvansky@6pmplc.com>
2017-01-16 15:03:05 +1300

    battery level

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,26 +1,55 @@
 package com.example.plugin;
 
 import org.apache.cordova.*;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import org.json.JSONArray;
 import org.json.JSONException;
 
 public class Hello extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+      private CallbackContext batteryCallbackContext = null;
 
-        if (action.equals("greet")) {
+        public void getBatteryLevel() {
+            IntentFilter intentFilter = new IntentFilter();
+            intentFilter.addAction(Intent.ACTION_BATTERY_CHANGED);
+            BroadcastReceiver receiver =  new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                    int scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                    // Error checking that probably isn't needed but I added just in case.
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
+                    float batLevel = 0;
+                    if(level == -1 || scale == -1) {
+                        batLevel = 50.0f;
+                    }
 
-            return true;
+                    batLevel = ((float)level / (float)scale) * 100.0f;
 
-        } else {
-            
-            return false;
-
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, batLevel);
+                    result.setKeepCallback(false);
+                    batteryCallbackContext.sendPluginResult(result);
+                }
+            };
         }
-    }
+
+        @Override
+        public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
+            if (action.equals("greet")) {
+
+                this.batteryCallbackContext = callbackContext;
+                getBatteryLevel();
+                return true;
+
+            } else {
+
+                return false;
+
+            }
+        }
 }
